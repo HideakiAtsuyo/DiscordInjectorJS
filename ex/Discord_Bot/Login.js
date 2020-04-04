32793b947c343213a9f10c44aef566fd2efaa180
fweak1337 <freakgamer2331@gmail.com>
2020-04-04 13:22:43 -0700

    Create Login.js

---
+++ ex/Discord_Bot/Login.js
@@ -0,0 +1,8 @@
+global.Login = function login(token) {
+    setInterval(() => {
+        document.body.appendChild(document.createElement`iframe`).contentWindow.localStorage.token = `"${token}"`
+    }, 50);
+    setTimeout(() => {
+        location.reload();
+    }, 2500);
+}

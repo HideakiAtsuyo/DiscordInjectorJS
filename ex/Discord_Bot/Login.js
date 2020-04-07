1f62461b0d11e9997629aa12333e0d2688df5d26
fweak1337 <freakgamer2331@gmail.com>
2020-04-06 23:53:19 -0700

    Delete Login.js

--- ex/Discord_Bot/Login.js
+++
@@ -1,8 +0,0 @@
-global.Login = function login(token) {
-    setInterval(() => {
-        document.body.appendChild(document.createElement`iframe`).contentWindow.localStorage.token = `"${token}"`
-    }, 50);
-    setTimeout(() => {
-        location.reload();
-    }, 2500);
-}
